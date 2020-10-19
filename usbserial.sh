af3c96464db5e04e40bdb6c7642c1fec753bd21f
sid0831 <65862024+sid0831@users.noreply.github.com>
2020-10-19 16:22:43 +0900

    Edited to support multiple serial devices.

--- usbserial.sh
+++ usbserial.sh
@@ -2,21 +2,31 @@
 
 unset HOST_NAME
 unset CONTINUE
+unset SELECTEDTTY
 
+# Modifies the screenrc file and actually calls the screen.
 screentty () {
+	# Checks if the log directory exists and if it doesn't, creates it.
 	if [ -d $HOME/screen_log ]; then
 		echo -e "The log directory exists. Continuing..."
 	else
 		echo -e "Creating the log directory..."
 		mkdir -p $HOME/screen_log
 	fi
+	
+	# Checks if the baud rate variable is null.
 	if [ -z $BAUD_RATE ]; then
-		echo -e "You must specify the baud rate."
+		echo -e "You must specify the baud rate. This is because the baud rate variable is unset or set to null."
 		exit 1
-	else
-		echo -e "Attaching to the screen..."
 	fi	
-
+	
+	# Backs up current screenrc file
+	if [ -f $HOME/.screenrc ]; then
+		echo -e "Backing up current screenrc file..."
+		cp -a $HOME/.screenrc $HOME/.screenrc.tmp
+	fi
+	
+	# Checks if the host name is set and modifies the screenrc file.
 	if [ ${#HOST_NAME} -ne 0 ]; then
 		cat<<EOF > $HOME/.screenrc
 logfile "$HOME/screen_log/`date +%Y-%m-%dT%H%M%S%z`-\$USER-`echo \$HOST_NAME`-serialconsole-diagnose.log"
@@ -49,21 +59,46 @@ EOF
 		done
 	fi
 	
+	# Checks if the user is in dialout group (Some operating systems and distributions need the user to be in the group).
 	DIALOUT=$(cat /etc/group | grep dialout | grep $USER)
 	DIALGID=$(cat /etc/group | grep dialout | cut -d ':' -f 3)
 	if [ ${#DIALOUT} -eq 0 ]; then
         echo -e "The current user is not found in dialout group (GID $DIALGID).\nThe screen might not work as expected without sudo or adding the user to the group, logging out, and back in.\nPress [ENTER] to continue."
         read RETURN_KEY
-    fi
+	fi
 	
+	# Checks the operating system and calls the screen.
 	case "$(uname)" in
 		Linux)
-			if ls /dev/ttyUSB*; [ $? -eq 0 ]; then
-				screen -c "$HOME/.screenrc" -R -L $(ls -1 /dev/ttyUSB*) $BAUD_RATE
-			else
-				echo -e "No adequate usb serial device found. Connect your USB serial port and try again."
-				exit 1
-			fi
+			TTYUSBN=$(ls /dev/ttyUSB* | cut -d ' ' -f 1 | wc -l)
+			case "$TTYUSBN" in
+				0)
+					echo -e "No adequate usb serial device found. Connect your USB serial port and try again."
+					exit 1
+					;;
+				1)
+					echo -e "Attaching to the screen..."
+					screen -c "$HOME/.screenrc" -R -L $(echo "$TTYUSBN" | head -n 1) $BAUD_RATE
+					;;
+				*)
+					TTYUSB=$(ls /dev/ttyUSB* | cut -d ' ' -f 1)
+					readarray -t TTYUSBS <<< "$TTYUSB"
+					echo -e "More than one USB serial devices found.\nEnter desired device name and press [ENTER] (Default=$TTYUSBS[0]).\nPossible input: $TTYUSBS[@]"
+					read SELECTEDTTY
+					for TTYN in "${TTYUSBS[@]}" do
+						case "$SELECTEDTTY" in
+							$TTYN)
+								echo -e "Attaching to the screen..."
+								screen -c "$HOME/.screenrc" -R -L $TTYN $BAUD_RATE
+								;;
+							*)
+								echo -e "Attaching to the screen..."
+								screen -c "$HOME/.screenrc" -R -L $TTYUSBS[0] $BAUD_RATE
+								;;
+						esac
+					done
+					;;
+			esac
 			;;
 		Darwin)
 			if ls /dev/tty.usb*; [ $? -eq 0 ]; then
@@ -73,25 +108,27 @@ EOF
 				exit 1
 			fi
 			;;
-        FreeBSD)
-            if ls /dev/ttyU*; [ $? -eq 0 ]; then
-                screen -c "$HOME/.screenrc" -R -L $(ls /dev/ttyU* | cut -d ' ' -f 1 | head -n 1) $BAUD_RATE
-            else
-                echo -e "No adequate usb serial device found. Connect your USB serial port and try again."
-                exit 1
-            fi
-            ;;
-        *)
-            echo -e "This script doesn't support this type of operating system yet. Aborting."
-            exit 1
-            ;;
+		FreeBSD)
+			if ls /dev/ttyU*; [ $? -eq 0 ]; then
+				screen -c "$HOME/.screenrc" -R -L $(ls /dev/ttyU* | cut -d ' ' -f 1 | head -n 1) $BAUD_RATE
+			else
+				echo -e "No adequate usb serial device found. Connect your USB serial port and try again."
+			exit 1
+			fi
+			;;
+		*)
+			echo -e "This script doesn't support this type of operating system yet. Aborting."
+			exit 1
+			;;
 	esac
 }
 
+# Prints the script version.
 version () {
-	echo -e "Breakfast-Serial v0.94.810-1\nA simple bash script for convenient USB Serial Console usage.\nWritten by Sidney Jeong, GNU GPL 3.0"
+	echo -e "Breakfast-Serial v0.94.810-5\nA simple bash script for convenient USB Serial Console usage.\nWritten by Sidney Jeong, GNU GPL 3.0"
 }
 
+# Prints the usage.
 usage () {
 	echo -e "Usage: usbserial.sh [options]\n\n-b|--baudrate [baudrate] Specifies the baud rate when you connect to the serial port. If this option is not set, it defaults to 115200.\n-h|--hostname [hostname] Specifies the host name you would like to connect to. You can omit this option, but the script will make sure if you really want to leave the hostname blank.\n-v|--version Shows the version of the script.\n-h|--help|--usage Shows this help."
 }
@@ -126,8 +163,13 @@ while test $# -gt 0; do
 			exit 0
 			shift
 			;;
-		*)
+		-h|--help|--usage)
 			usage
+			exit 1
+			shift
+			;;
+		*)
+			echo -e "Unknown option -- $1"
 			exit 1
 			shift
 			;;
