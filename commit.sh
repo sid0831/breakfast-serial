36834077b608f9442c3f46d692be035bfee75e34
Sidney Jeong <65862024+sid0831@users.noreply.github.com>
2020-10-27 06:11:12 +0900

    Version marking change.

--- commit.sh
+++ commit.sh
@@ -2,7 +2,6 @@
 
 VERSINFO=$(cat $PWD/usbserial.sh | grep -iE 'Breakfast-Serial v' | sed -E 's/([ \t].*)(echo -e "Breakfast-Serial v)(.*)(\\nA.*)/\3/g')
 VERSARRAY=( $(echo "$VERSINFO" | sed -E 's/([0-9]{1})(\.)([0-9]{2})(\.)([0-9]{3})(\-)([0-9]{1})(\.)([0-9]{2})/\1/') $(echo "$VERSINFO" | sed -E 's/([0-9]{1})(\.)([0-9]{2})(\.)([0-9]{3})(\-)([0-9]{1})(\.)([0-9]{2})/\3/') $(echo "$VERSINFO" | sed -E 's/([0-9]{1})(\.)([0-9]{2})(\.)([0-9]{3})(\-)([0-9]{1})(\.)([0-9]{2})/\5/') $(echo "$VERSINFO" | sed -E 's/([0-9]{1})(\.)([0-9]{2})(\.)([0-9]{3})(\-)([0-9]{1})(\.)([0-9]{2})/\7/') $(echo "$VERSINFO" | sed -E 's/([0-9]{1})(\.)([0-9]{2})(\.)([0-9]{3})(\-)([0-9]{1})(\.)([0-9]{2})/\9/') )
-COMMITCOUNT=$(git rev-list --count main)
 
 commitcode () {
   local QMARK=1
@@ -12,6 +11,7 @@ commitcode () {
 
 verschange () {
   local QMARK=1
+  local COMMITCOUNT=$(git rev-list --count main)
   ${VERSARRAY[4]}=$(( $COMMITCOUNT + 1 ))
   echo -e "Marking new version...\nOLD: v$VERSINFO\nNEW: v${VERSARRAY[0]}.${VERSARRAY[1]}.${VERSARRAY[2]}-${VERSARRAY[3]}.${VERSARRAY[4]}"
   sed -E -i "s/([0-9]{1})(\.)([0-9]{2})(\.)([0-9]{3})(\-)([0-9]{1})(\.)([0-9]{2})/${VERSARRAY[0]}.${VERSARRAY[1]}.${VERSARRAY[2]}\-${VERSARRY[3]}.${VERSARRAY[4]}/g" $PWD/usbserial.sh; QMARK=$?
